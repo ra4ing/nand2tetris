d2fe4ff487f8ba51be945cfe786ca34f61d30e5b
ra4ing <ra4ing@gmail.com>
2023-09-25 16:42:15 +0100

    mult

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -4,9 +4,30 @@
 // File name: projects/04/Mult.asm
 
 // Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-//
-// This program only needs to handle arguments that satisfy
-// R0 >= 0, R1 >= 0, and R0*R1 < 32768.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
-// Put your code here.
+    @2
+    M=0     // R2 = 0
+    @0
+    D=M
+    @i
+    M=D     // i=R0
+(LOOP)
+    @i
+    D=M     // D=i
+    @END
+    D;JLE    // if i-R0 >= 0: jmp END
+
+    @i
+    M=M-1   // else: i=i-1
+
+    @1
+    D=M     // D=R1
+    @2
+    M=D+M   // R2=R2+R1
+    
+    @LOOP
+    0;JMP   // Repeat
+(END)
+    @END
+    0;JMP
